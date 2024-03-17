0b33e4f680f36f705c2398d686c27e4f2e612625
GerarddeTena <156508398+GerarddeTena@users.noreply.github.com>
2024-03-17 10:09:00 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector("#navbar");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", () => {
+   nav.classList.add("visible");
+})
+close.addEventListener("click", () =>{
+   nav.classList.remove("visible");
+})
